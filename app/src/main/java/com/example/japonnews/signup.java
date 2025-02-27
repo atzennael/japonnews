db6d19caf504ba7e19eb868e64465322c15e0585
unknown <aedra2019@gmail.com>
2025-02-27 16:42:53 -0500

    Inicio de sesión y registro completados 100%

--- app/src/main/java/com/example/japonnews/signup.java
+++ app/src/main/java/com/example/japonnews/signup.java
@@ -2,29 +2,40 @@ package com.example.japonnews;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.example.japonnews.conexion.MySQLConexion;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.japonnews.ui.login.LoginActivity;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class signup extends AppCompatActivity {
 
     private EditText editTextNombre;
     private EditText editTextEmail;
     private EditText editTextPassword;
+    private EditText editTextIdentificacion;
     private FirebaseAuth auth;
+    private ImageView imgProfile;
+    private Uri imgUri;
+    private Button btnCargarImg;
+    private Button btnCrearCuenta;
+    private static final int PICK_IMAGE_REQUEST =1;
+
 
     @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
@@ -34,21 +45,30 @@ public class signup extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         auth = FirebaseAuth.getInstance();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         editTextNombre = findViewById(R.id.editTextText);
-        EditText editTextIdentificacion = findViewById(R.id.editTextNumber);
+        editTextIdentificacion = findViewById(R.id.editTextNumber);
         editTextEmail = findViewById(R.id.editTextTextEmailAddress);
         editTextPassword = findViewById(R.id.editTextPassword);
-        Button btnCrearCuenta = findViewById(R.id.login3);
+        btnCrearCuenta = findViewById(R.id.login3);
+        imgProfile = findViewById(R.id.imageView2);
+        btnCargarImg = findViewById(R.id.cargarImg);
 
-        btnCrearCuenta.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("SignupActivity", "Botón Crear Cuenta presionado");
-                Toast.makeText(signup.this, "Botón presionado", Toast.LENGTH_SHORT).show();
-                registrarUsuario();
-            }
+        if (btnCargarImg == null) {
+            Log.e("SignupActivity", "ERROR: btnCargarImg es NULL. Revisa el ID en XML.");
+        } else {
+            Log.d("SignupActivity", "btnCargarImg encontrado correctamente.");
+        }
+
+        btnCargarImg.setOnClickListener(v -> {
+            Log.d("SignupActivity", "Botón de cargar imagen presionado");
+            Toast.makeText(signup.this, "Botón presionado", Toast.LENGTH_SHORT).show();
+            cargarImagen();
         });
+
+
+        btnCrearCuenta.setOnClickListener(v -> registrarUsuario());
 
         TextView textview = findViewById(R.id.textView7);
         textview.setOnClickListener(new View.OnClickListener() {
@@ -67,8 +87,9 @@ public class signup extends AppCompatActivity {
         String nombre = editTextNombre.getText().toString().trim();
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
+        String id = editTextIdentificacion.getText().toString().trim();
 
-        if (nombre.isEmpty() || email.isEmpty() || password.isEmpty()) {
+        if (nombre.isEmpty() || email.isEmpty() || password.isEmpty() || id.isEmpty()) {
             Toast.makeText(this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -83,25 +104,72 @@ public class signup extends AppCompatActivity {
             return;
         }
 
-        if (auth == null) {
-            Log.e("SignupActivity", "FirebaseAuth es null. Revisa la inicialización.");
-        } else {
-            Log.d("SignupActivity", "FirebaseAuth inicializado correctamente.");
-        }
         auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
                         FirebaseUser user = auth.getCurrentUser();
                         if (user != null) {
-                            String firebaseUID = user.getUid();
-                            MySQLConexion.registrarUsuario(this, firebaseUID, nombre, email, null);
-                            Toast.makeText(this, "Usuario registrado con éxito", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(signup.this, LoginActivity.class));
-                            finish();
+                            String userId = user.getUid();
+
+                            Map<String, Object> userData = new HashMap<>();
+                            userData.put("uid", userId);
+                            userData.put("nombre", nombre);
+                            userData.put("id", id);
+                            userData.put("email", email);
+                            userData.put("password", password);
+                            userData.put("fotoPerfil", null);
+
+                            FirebaseFirestore.getInstance().collection("usuarios").document(userId)
+                            .set(userData)
+                                    .addOnSuccessListener(aVoid -> {
+                                        guardarImg(userId);
+                                        Toast.makeText(this, "Usuario registrado con éxito", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(signup.this, LoginActivity.class));
+                                        finish();
+                                            })
+                                    .addOnFailureListener(e -> Log.e("SignupActivity", "Error al guardar usuario", e));
                         }
+
                     } else {
                         String errorMsg = task.getException() != null ? task.getException().getMessage() : "Error desconocido";
-                        Log.e("SignupActivity", "Error en el registro: " + errorMsg);                    }
+                        Log.e("SignupActivity", "Error en el registro: " + errorMsg);
+                        Toast.makeText(signup.this, "Error al guardar usuario", Toast.LENGTH_SHORT).show();
+                        }
                 });
+    }
+
+    private void cargarImagen(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==PICK_IMAGE_REQUEST&&resultCode==RESULT_OK&&data != null&&data.getData()!=null){
+            imgUri = data.getData();
+            imgProfile.setImageURI(imgUri);
+            Log.d("SignupActivity", "Imagen seleccionada: " + imgUri.toString());
+        } else{
+            Log.e("SignupActivity", "No se seleccionó ninguna imagen o hubo un error");
+        }
+    }
+
+    private void guardarImg(String userId){
+        if (imgUri!=null){
+            StorageReference fileRef = FirebaseStorage.getInstance().getReference("profile_pics")
+                    .child(userId+".jpg");
+            fileRef.putFile(imgUri)
+                    .addOnSuccessListener(taskSnapshot -> fileRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                        String downloadUrl = uri.toString();
+                        FirebaseFirestore.getInstance().collection("usuarios").document(userId)
+                                .update("fotoPerfil", downloadUrl)
+                                .addOnSuccessListener(aVoid -> Log.d("SignupActivity", "Foto de perfil guardada"))
+                                .addOnFailureListener(e -> Log.e("SignupActivity","Error: ", e));
+                    }))
+                    .addOnFailureListener(e-> Log.e("SignupActivity", "Error al subir la imagen: ", e));
+        }
     }
 }
--- app/src/main/java/com/example/japonnews/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/japonnews/ui/login/LoginActivity.kt
@@ -11,10 +11,7 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.example.japonnews.R
 import com.example.japonnews.databinding.ActivityLoginBinding
 import com.example.japonnews.home1
@@ -22,14 +19,13 @@ import com.example.japonnews.signup
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.OAuthProvider
-import com.google.firebase.firestore.FirebaseFirestore
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var loginViewModel: LoginViewModel
+    // private lateinit var loginViewModel: LoginViewModel
     private lateinit var binding: ActivityLoginBinding
     private lateinit var auth: FirebaseAuth
-    private lateinit var db: FirebaseFirestore
+    //private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,24 +34,31 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         auth = FirebaseAuth.getInstance()
-        db = FirebaseFirestore.getInstance()
         val currentUser: FirebaseUser? = auth.currentUser
 
         if (currentUser != null) {
-            startActivity(Intent(this, home1::class.java))
-            finish()
+            goToHome()
             return
         }
-        val outlook = findViewById <ImageView>(R.id.outlook)
+        val outlook = findViewById<ImageView>(R.id.outlook)
         outlook.setOnClickListener {
             signInWithMicrosoft()
         }
 
-        val login = binding.login
+        binding.login.setOnClickListener {
+            val email = binding.username.text.toString().trim()
+            val password = binding.password.text.toString().trim()
 
-        login.setOnClickListener {
-            binding.loading.visibility = View.VISIBLE
-            checkUserExists(binding.username.text.toString(), binding.password.text.toString())
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Por favor, ingresa tu correo y contraseña",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+            binding.loading.visibility = android.view.View.VISIBLE
+            loginUser(email, password)
         }
 
         val textView = findViewById<TextView>(R.id.textView2)
@@ -63,36 +66,35 @@ class LoginActivity : AppCompatActivity() {
             val signupIntent = Intent(this, signup::class.java)
             startActivity(signupIntent)
         }
-    }
 
-    private fun checkUserExists(email: String, password: String) {
-        db.collection("usuarios").document(email).get()
-            .addOnSuccessListener { document ->
-                if (document.exists()) {
-                    loginUser(email, password)
-                } else {
-                    binding.loading.visibility = View.GONE
-                    Toast.makeText(this, "El usuario no está registrado", Toast.LENGTH_SHORT).show()
-                }
+        binding.password.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                binding.login.performClick()
+                return@setOnEditorActionListener true
             }
-            .addOnFailureListener {
-                binding.loading.visibility = View.GONE
-                Toast.makeText(this, "Error al verificar usuario", Toast.LENGTH_SHORT).show()
-            }
+            false
+        }
     }
-
     private fun loginUser(email: String, password: String) {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 binding.loading.visibility = View.GONE
                 if (task.isSuccessful) {
-                    val home = Intent(this, home1::class.java)
-                    startActivity(home)
-                    finish()
+                    Log.d("LoginActivity", "Inicio de sesión exitoso")
+                    Toast.makeText(this, "Bienvenido a Japon News", Toast.LENGTH_SHORT).show()
+                    goToHome()
                 } else {
-                    Toast.makeText(this, "Usuario y/o contraseña incorrecto", Toast.LENGTH_SHORT).show()
+                    val errorMsg = task.exception?.localizedMessage ?: "Error desconocido"
+                    Log.e("LoginActivity", "Error en inicio de sesión: $errorMsg")
+                    Toast.makeText(this, "Error: $errorMsg", Toast.LENGTH_LONG).show()
                 }
             }
+    }
+
+    private fun goToHome() {
+        val intent = Intent(this, home1::class.java)
+        startActivity(intent)
+        finish()
     }
 
     private fun signInWithMicrosoft() {
@@ -128,14 +130,6 @@ class LoginActivity : AppCompatActivity() {
                 }
         }
     }
-
-    private fun goToHome() {
-        val intent = Intent(this, home1::class.java)
-        startActivity(intent)
-        finish()
-    }
-
-
 }
 
 fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
